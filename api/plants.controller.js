afc28886446bc80e40ad8ec8487ddd546e73f543
Meyrav Dayan <meyrav.dayan@gmail.com>
2023-06-11 21:56:51 +0300

    Update plants.controller.js

--- api/plants.controller.js
+++ api/plants.controller.js
@@ -104,7 +104,7 @@ export default class PlantsController {
                 }
             });
             // newPlantProfile.dateCreated = new Date().toJSON().slice(0, 10);
-            if ("prefrencedWater" in filteredPlantProfile) {
+            if (filteredPlantProfile.includes("prefrencedWater")) {
                 newPlantProfile[waterFrequencyScores] =
                     wateringFrequencyDaysValues[
                         newPlantProfile.prefrences.water
